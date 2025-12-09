7e7b3696965e1b252a2f7466936928314e3a52a1
Ana Clara Silva <ana.silva.ifpr.2023@gmail.com>
2025-12-09 19:47:30 -0300

    basicofuncionando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-resumo();
+
+exibir();
 
 /*Cadastrar Salário */
 document.getElementById("casl").addEventListener("click", function () {
@@ -16,18 +17,22 @@ document.getElementById("casl").addEventListener("click", function () {
     alert("Salário cadastrado.");
     document.getElementById("salario").value = "";
 
+    exibir()
+    resumo_Financeiro()
+
+
 });
 
 
 /*Cadastrar Despesa */
-document.getElementById("formDespesa").addEventListener("submit", function (event) {
+document.getElementById("formDespesas").addEventListener("submit", function (event) {
     event.preventDefault();
 
     var nome = document.getElementById("nome").value
     var data = document.getElementById("data").value
     var valor = document.getElementById("valor").value
 
-    if (data === "" || descricao === "" || isNaN(valor) || valor === "") {
+    if (data === "" || nome === "" || isNaN(valor) || valor === "") {
         alert("Preencha todos os campos de modo correto.");
         return;
     }
@@ -39,11 +44,22 @@ document.getElementById("formDespesa").addEventListener("submit", function (even
         /*Cria um objeto */
     }
 
-    var listadespesas = JSON.parse(localStorage.getItem("lista")) || []
-    listadespesas.push(despesa) 
-    localStorage.setItem("lista", JSON.stringify(listadespesas))
-    document.getElementById("formulario").reset() 
-    exibirDespesas() 
+    var listaDespesas = JSON.parse(localStorage.getItem("lista")) || [];
+    listaDespesas.push(despesa);
+    localStorage.setItem("lista", JSON.stringify(listaDespesas));
+    document.getElementById("formDespesas").reset();
 
+    exibir();
+    resumo_Financeiro();
 });
 
+function exibir() {
+    var listaDespesas = JSON.parse(localStorage.getItem('lista')) || []
+    var output = document.getElementById("output")
+    output.innerHTML = ""
+    for (let i = 0; i < listaDespesas.length; i++) {
+        let li = document.createElement('li')
+        li.textContent = 'Data: ' + listaDespesas[i].data + ' Nome: ' + listaDespesas[i].nome + ' Valor: R$' + listaDespesas[i].valor
+        output.appendChild(li)
+    }
+}
